c9bab3cd024b02416b18e06bfeccacc4e8ba1f6b
Владимир Самойлов <shadow757.shadow@yandex.ru>
2025-07-22 17:13:53 +0500

    CI/CD

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+global.clearImmediate = global.clearImmediate || ((t) => clearTimeout(t));
+global.setImmediate = global.setImmediate || ((cb, ...args) => setTimeout(cb, 0, ...args));
